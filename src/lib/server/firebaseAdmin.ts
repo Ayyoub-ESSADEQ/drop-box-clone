ea7b09cfc04ad0f4b0b3ee93c0bc86aa28e35b25
Ayyoub-ESSADEQ <ayoube.essadeq@gmail.com>
2024-06-01 16:39:23 +0100

    revoke changes

--- src/lib/server/firebaseAdmin.ts
+++ src/lib/server/firebaseAdmin.ts
@@ -1,13 +1,13 @@
 import { getAuth } from 'firebase-admin/auth';
-// import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
+import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 import pkg from 'firebase-admin';
 
 try {
 	pkg.initializeApp({
 		credential: pkg.credential.cert({
-			projectId: process.env.FB_PROJECT_ID,
-			clientEmail: process.env.FB_CLIENT_EMAIL,
-			privateKey: process.env.FB_PRIVATE_KEY
+			projectId: FB_PROJECT_ID,
+			clientEmail: FB_CLIENT_EMAIL,
+			privateKey: FB_PRIVATE_KEY
 		})
 	});
 } catch (e) {
